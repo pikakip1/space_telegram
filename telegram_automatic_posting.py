4e7831cd3762c58d5cf6a5fb449ab8c3207ab2c2
pikakip1 <pikakip222@mail.ru>
2023-07-09 20:04:49 +0300

    Добавил возможность передачи задержи через файл

--- telegram_automatic_posting.py
+++ telegram_automatic_posting.py
@@ -3,10 +3,16 @@ import random
 import argparse
 from dotenv import load_dotenv
 from time import sleep
-from telegramm_bot import telegramm_photo_post
+import telegram
 
 
-def auto_post(bot_token, directory, delay):
+def telegramm_photo_post(token, chat_id, path):
+    bot = telegram.Bot(token=token)
+
+    bot.send_document(chat_id=chat_id, document=open(path, 'rb'))
+
+
+def get_photo_path(bot_token,chat_id, directory, delay):
     file = os.listdir(directory)
     file_number = len(file)
     index = 0
@@ -15,7 +21,7 @@ def auto_post(bot_token, directory, delay):
         if index == file_number:
             random.shuffle(file)
             index = 0
-        telegramm_photo_post(bot_token, f'{directory}/{file[index]}')
+        telegramm_photo_post(bot_token, chat_id, f'{directory}/{file[index]}')
         index += 1
         sleep(delay)
 
@@ -23,25 +29,27 @@ def auto_post(bot_token, directory, delay):
 def main():
     parser = argparse.ArgumentParser(description='''
     Скрипт выкладывает фото в телеграмм.
+    Требует обязательный аргумент, id чата.
     По умолчанию, фото берутся из папки nasa_apod.
     Задержка по умолчанию, 4 часа(3.600 сек).
     Задержка устанавливается в секундах.
     ''')
+
+    parser.add_argument('chat_id')
     parser.add_argument('-d', '--directory', default='nasa_apod')
     parser.add_argument('-t', '--delay')
     args = parser.parse_args()
 
     load_dotenv('TELEGRAMM_TOKEN.env')
     bot_token = os.environ['TELEGRAMM_KEY']
-
-    load_dotenv('POST_DELAY.env')
-    post_delay = os.environ['post_delay']
     delay = args.delay
 
     if not args.delay:
-        delay = int(post_delay)
+        file_delay = open('POST_DELAY.txt')
+        delay = file_delay.read()
+        file_delay.close()
 
-    auto_post(bot_token, args.directory, int(delay))
+    get_photo_path(bot_token,args.chat_id, args.directory, int(delay))
 
 
 if __name__ == '__main__':
