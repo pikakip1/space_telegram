ac4d49cd33e6eabfb5ed0512aac2d0228b6242c9
pikakip1 <pikakip222@mail.ru>
2023-07-09 11:40:17 +0300

    publish photo to Telegram channel

--- telegramm_bot.py
+++ telegramm_bot.py
@@ -5,5 +5,6 @@ load_dotenv('TELEGRAMM_TOKEN.env')
 token = os.environ['TELEGRAMM_KEY']
 
 bot = telegram.Bot(token=token)
-bot.send_message(chat_id='@cosmo_photos', text='Привет космическим подписчикам!!!')
 
+bot.send_message(chat_id='@cosmo_photos', text='Первое фото')
+bot.send_document(chat_id='@cosmo_photos', document=open('nasa_apod/apod1.gif', 'rb'))
