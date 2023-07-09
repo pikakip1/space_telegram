3bceed54e3be868b40d3e14260a724ef9d45742f
pikakip1 <pikakip222@mail.ru>
2023-07-09 13:52:55 +0300

    changed the location of the token

--- telegramm_bot.py
+++ telegramm_bot.py
@@ -1,10 +1,8 @@
-import os
-from dotenv import load_dotenv
 import telegram
-load_dotenv('TELEGRAMM_TOKEN.env')
-token = os.environ['TELEGRAMM_KEY']
 
-bot = telegram.Bot(token=token)
 
-bot.send_message(chat_id='@cosmo_photos', text='Первое фото')
-bot.send_document(chat_id='@cosmo_photos', document=open('nasa_apod/apod1.gif', 'rb'))
+def telegramm_photo_post(token, path):
+    bot = telegram.Bot(token=token)
+
+    bot.send_document(chat_id='@cosmo_photos', document=open(path, 'rb'))
+
