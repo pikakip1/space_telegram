f4036eb47d23a0e7c1eccf87eeed5378382d72c6
pikakip1 <pikakip222@mail.ru>
2023-07-14 19:14:19 +0300

    Split the function into parts

--- photo_download.py
+++ photo_download.py
@@ -4,20 +4,30 @@ import re
 from os.path import splitext
 
 
+def create_folder(img_way):
+    os.makedirs(img_way, exist_ok=True)
+
+
+def count_photo(img_way):
+    return len(os.listdir(img_way))
+
+
+def get_extension(url):
+    enlargement = splitext(url)[-1]
+
+    if not (enlargement.strip('.')).isalpha():
+        pattern = '(\.[a-z]+)'
+        enlargement = re.search(pattern, enlargement).group(0)
+    return enlargement
+
+
 def image_download(url, img_way, img_name):
-    if not os.path.isdir(img_way):
-        os.mkdir(img_way)
+    create_folder(img_way)
+    response = requests.get(url)
+    enlargement = get_extension(url)
+    number_photo = count_photo(img_way)
 
-    files_count = len(os.listdir(img_way))
+    with open(f'{img_way}/{img_name}{number_photo}{enlargement}', 'wb') as file:
+        file.write(response.content)
 
-    for number, url in enumerate(url, files_count):
-        response = requests.get(url)
-        enlargement = splitext(url)[-1]
-
-        if not (enlargement.strip('.')).isalpha():
-            pattern = '(\.[a-z]+)'
-            enlargement = re.search(pattern, enlargement).group(0)
-
-        with open(f'{img_way}/{img_name}{number}{enlargement}', 'wb') as file:
-            file.write(response.content)
 
