c5b0ce91b1eeb1cca045843b6562b126f8fdc22a
pikakip1 <pikakip222@mail.ru>
2023-07-14 19:13:51 +0300

    changed variable names, removed unnecessary queries

--- downoad_nasa_epic_image.py
+++ downoad_nasa_epic_image.py
@@ -9,23 +9,22 @@ import os
 def get_epic_image(token, photo_count):
     url = 'https://epic.gsfc.nasa.gov/api/natural'
     response = requests.get(url)
-    photo_url = []
+    response.raise_for_status()
+    photo_urls = []
 
-    for index, file_json in enumerate(response.json(), 1):
-        date = datetime.fromisoformat(file_json['date']).strftime('%Y/%m/%d')
-        photo_id = file_json['identifier']
-        photo_url.append([date, photo_id])
+    for index, reply in enumerate(response.json(), 1):
+        date = datetime.fromisoformat(reply['date']).strftime('%Y/%m/%d')
+        photo_id = reply['identifier']
+        photo_urls.append([date, photo_id])
         if index == photo_count:
             break
 
     payload = {'api_key': token}
 
     urls = []
-    for data, photo_id in photo_url:
-        url = f'https://api.nasa.gov/EPIC/archive/natural/{data}/png/epic_1b_{photo_id}.png'
-
-        response = requests.get(url, params=payload)
-        urls.append(response.url)
+    for date, photo_id in photo_urls:
+        url = f'https://api.nasa.gov/EPIC/archive/natural/{date}/png/epic_1b_{photo_id}.png?api_key={token}'
+        urls.append(url)
     return urls
 
 
@@ -37,14 +36,17 @@ def main():
                                      Агумент --file_name, отвечает за имя директории(по умолчанию nasa_epic).\n
                                      Аргумент --photo_name, отвечает за название фото(по умолчанию epic)'''
                                      )
-    parser.add_argument('-c', '--count_photo', default=1)
-    parser.add_argument('-f', '--file_name', default='spacex_fetch')
-    parser.add_argument('-p', '--photo_name', default='spacex')
+    parser.add_argument('-c', '--count_photo', default=3)
+    parser.add_argument('-f', '--file_name', default='epic_nasa')
+    parser.add_argument('-p', '--photo_name', default='epic')
     args = parser.parse_args()
 
     load_dotenv('NASA_TOKEN.env')
     token = os.environ['NASA_KEY']
-    image_download(get_epic_image(token, args.count_photo), args.file_name, args.photo_name)
+
+    nasa_epic_urls = get_epic_image(token, args.count_photo)
+    for nasa_epic_url in nasa_epic_urls:
+        image_download(nasa_epic_url, args.file_name, args.photo_name)
 
 
 if __name__ == '__main__':
