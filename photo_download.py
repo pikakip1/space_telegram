3018be55c0734dfcf5475e92fe65e2429f5cfeb7
pikakip1 <pikakip222@mail.ru>
2023-07-19 12:51:18 +0300

    removed unnecessary functions

--- photo_download.py
+++ photo_download.py
@@ -4,30 +4,20 @@ import re
 from os.path import splitext
 
 
-def create_folder(img_way):
-    os.makedirs(img_way, exist_ok=True)
-
-
-def count_photo(img_way):
-    return len(os.listdir(img_way))
-
-
 def get_extension(url):
     enlargement = splitext(url)[-1]
 
     if not (enlargement.strip('.')).isalpha():
-        pattern = '(\.[a-z]+)'
+        pattern = r'(\.[a-z]+)'
         enlargement = re.search(pattern, enlargement).group(0)
     return enlargement
 
 
-def image_download(url, img_way, img_name):
-    create_folder(img_way)
+def image_download(url, img_path, img_name):
+    os.makedirs(img_path, exist_ok=True)
     response = requests.get(url)
     enlargement = get_extension(url)
-    number_photo = count_photo(img_way)
+    number_photo = len(os.listdir(img_path))
 
-    with open(f'{img_way}/{img_name}{number_photo}{enlargement}', 'wb') as file:
+    with open(f'{img_path}/{img_name}{number_photo}{enlargement}', 'wb') as file:
         file.write(response.content)
-
-
